0501261c5b88fb10b08ec91289067958462e233c
ChSunayReddy <sunayreddyc@gmail.com>
2025-09-08 16:47:15 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import {BrowserRouter , Routes , Route} from 'react-router-dom';
 import Login from './pages/Login'
 import Register from './pages/Register'
 import { Toaster } from 'react-hot-toast';
-import {Button} from 'antd';
 import Home from "./pages/Home";
 import { useSelector } from "react-redux";
 import ProtectedRoutes from "./components/ProtectedRoutes";
