0f80d6ef7448fe821bf2aba44b3a77397739dc25
ChSunayReddy <sunayreddyc@gmail.com>
2025-09-08 17:31:13 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React from 'react';
 import 'antd/dist/reset.css';
 import ReactDOM from 'react-dom/client';
@@ -6,7 +7,7 @@ import {Provider} from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
-
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || "";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
