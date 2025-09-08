ff91bd549b5e78470c05b78a163d529656a35c5a
ChSunayReddy <sunayreddyc@gmail.com>
2025-09-08 17:04:40 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import {Button, Form , Input} from 'antd';
 import { Link,useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
-import {useSelector,useDispatch} from "react-redux";
+import {useDispatch} from "react-redux";
 import { setUser } from "../redux/userSlice";
 import React from "react";
 import axios from 'axios';
